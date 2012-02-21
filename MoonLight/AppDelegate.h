21ca4596cfdbf28f0c7686b6c19b4efba08f5613
chen Jaykie <chenjaykie@chen-JaykiematoMacBook-Pro.local>
2012-02-21 09:02:36 +0800

    Initial Commit

---
+++ MoonLight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoonLight
+//
+//  Created by chen Jaykie on 12-2-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
